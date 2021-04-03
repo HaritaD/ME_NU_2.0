4c2a5f92e27c6cc216ee70d862b4ae780f7936fa
HaritaD <haritajee@gmail.com>
2021-04-03 16:08:36 -0500

    Added all drop downs

--- src/Profile.js
+++ src/Profile.js
@@ -27,6 +27,96 @@ const dietitems = [
     value: 'Vegan',
   },
 ];
+const allergies = [
+  {
+    id: 1,
+    value: 'Dairy',
+  },
+  {
+    id: 2,
+    value: 'Nuts',
+  },
+  {
+    id: 3,
+    value: 'Shellfish',
+  },
+  {
+    id: 4,
+    value: 'Eggs',
+  },
+  {
+    id: 5,
+    value: 'Soy',
+  },
+];
+const DiningHall = [
+  {
+    id: 1,
+    value: 'Allison',
+  },
+  {
+    id: 2,
+    value: 'Sargent',
+  },
+  {
+    id: 3,
+    value: 'Elder',
+  },
+  {
+    id: 4,
+    value: 'Foster Walker',
+  },
+]
+  const NumSlots = [
+    {
+      id: 1,
+      value: 1,
+    },
+    {
+      id: 2,
+      value: 2,
+    },
+    {
+      id: 3,
+      value: 3,
+    },
+    {
+      id: 4,
+      value: 4,
+
+    },
+    {
+      id: 5,
+      value: 5,
+      
+    },
+  
+];
+const NumItems = [
+  {
+    id: 1,
+    value: 1,
+  },
+  {
+    id: 2,
+    value: 2,
+  },
+  {
+    id: 3,
+    value: 3,
+  },
+  {
+    id: 4,
+    value: 4,
+
+  },
+  {
+    id: 5,
+    value: 5,
+    
+  },
+
+];
 
 function Profile() {
   return (
@@ -39,29 +129,51 @@ function Profile() {
           🎥
         </span>
       </h1>
-      <Dropdown title="Select movie" items={dietitems} multiSelect />
+      <Dropdown title="Select Diet" items={dietitems} multiSelect />
     </div>
     </div>
     <div>
     <div className="container">
       <h1 style={{ textAlign: 'center' }}>
-        Buy Movies{' '}
+        Allergies{' '}
         <span role="img" aria-label="Movie projector">
           🎥
         </span>
       </h1>
-      <Dropdown title="Select movie" items={dietitems} multiSelect />
+      <Dropdown title="Select Allergies" items={allergies} multiSelect />
     </div>
     </div>
     <div>
     <div className="container">
       <h1 style={{ textAlign: 'center' }}>
-        Buy Movies{' '}
+        Dining Hall{' '}
         <span role="img" aria-label="Movie projector">
           🎥
         </span>
       </h1>
-      <Dropdown title="Select movie" items={dietitems} multiSelect />
+      <Dropdown title="Select Dining Hall" items={DiningHall} Select />
+    </div>
+    </div>
+    <div>
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>
+        Number of Meals{' '}
+        <span role="img" aria-label="Movie projector">
+          🎥
+        </span>
+      </h1>
+      <Dropdown title="Select Preferred # of Meals" items={NumSlots} Select />
+    </div>
+    </div>
+    <div>
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>
+        Number of Items{' '}
+        <span role="img" aria-label="Movie projector">
+          🎥
+        </span>
+      </h1>
+      <Dropdown title="Select Preferred # of Items/Meal" items={NumItems} Select />
     </div>
     </div>
     </div>
