09e13752692b527941d16e94b0c508add26597c7
Sengdao <sengdao.inthavong3@gmail.com>
2021-04-09 11:47:20 -0500

    Modify home page button

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className='bg-white h-screen flex flex-col justify-center items-center'>
-      <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>
+      <h1 className='-mt-36 lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-4'>
         MeNU
       </h1>
       <Link 
         to='/profile'
       >
-        <button className = "transform hover:scale-105 duration-300 ease-in-out" >Build Your Profile</button>
+        <button className = "p-4 shadow-md font-semibold border-black rounded-xl border-2 transform hover:scale-105 duration-300 ease-in-out" >Build Your Profile</button>
         
         
       </Link>
