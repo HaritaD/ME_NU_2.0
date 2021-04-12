b1bccd1c08729d506b945b11f2d2d0f6b63cacbd
Sengdao <sengdao.inthavong3@gmail.com>
2021-04-12 10:18:36 -0500

    Add macros

--- src/Recommender.js
+++ src/Recommender.js
@@ -8,7 +8,7 @@ function Recommender() {
   return (
     <section class="text-gray-600 body-font overflow-hidden">
   <div class="container px-5 py-12 mx-auto">
-    <div class="border-1 bg-white rounded-2xl py-12 flex flex-col text-center w-full mb-20">
+    <div class="border-2 border-gray-100 rounded-2xl py-12 flex flex-col text-center w-full mb-20">
       <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Recommender</h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Let me suggest something for you to eat at <a className="font-semibold">Sargent</a>. I promise it'll be decent!</p>
     </div>
@@ -16,7 +16,16 @@ function Recommender() {
       <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
         <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
           <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Breakfast</h2>
-          <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{b_sums[0]+b_sums[1]+b_sums[2]}<a className = "text-xl">cal</a></h1>
+          <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{b_sums[0]}<a className = "text-xl"> cal</a></h1>
+          <p className = "mt-1 text-md">
+            Protein: {b_sums[1]}g
+          </p>
+          <p className = "text-md">
+            Carbs: {b_sums[2]}g
+          </p>
+          <p className = "mb-5 text-md">
+            Fat: {b_sums[3]}g
+          </p>
           <p class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
@@ -38,6 +47,7 @@ function Recommender() {
               </svg>
             </span>{b_foods[2]}
           </p>
+
           <button class="flex items-center mt-auto text-white bg-purple-800 border-0 py-2 px-4 w-full focus:outline-none hover:bg-purple-900 rounded">Servings
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -49,7 +59,16 @@ function Recommender() {
       <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
         <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
           <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Lunch</h2>
-          <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{l_sums[0]+l_sums[1]+l_sums[2]}<a className = "text-xl">cal</a></h1>
+          <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{l_sums[0]}<a className = "text-xl"> cal</a></h1>
+          <p className = "mt-1 text-md">
+            Protein: {l_sums[1]}g
+          </p>
+          <p className = "text-md">
+            Carbs: {l_sums[2]}g
+          </p>
+          <p className = "mb-5 text-md">
+            Fat: {l_sums[3]}g
+          </p>
           <p class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
@@ -82,7 +101,16 @@ function Recommender() {
       <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
         <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
           <h2 class="text-sm tracking-widest title-font mb-1 font-medium">Dinner</h2>
-          <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{d_sums[0]+d_sums[1]+d_sums[2]}<a className = "text-xl">cal</a></h1>
+          <h1 class="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{d_sums[0]}<a className = "text-xl"> cal</a></h1>
+          <p className = "mt-1 text-md">
+            Protein: {d_sums[1]}g
+          </p>
+          <p className = "text-md">
+            Carbs: {d_sums[2]}g
+          </p>
+          <p className = "mb-5 text-md">
+            Fat: {d_sums[3]}g
+          </p>
           <p class="flex items-center text-gray-600 mb-2">
             <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
@@ -116,7 +144,7 @@ function Recommender() {
         <div class="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
           <span class="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">Total</span>
           <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-          {b_sums[0]+b_sums[1]+b_sums[2]+l_sums[0]+l_sums[1]+l_sums[2]+d_sums[0]+d_sums[1]+d_sums[2]}<a className = "px-2 text-xl">calories</a>
+          {b_sums[0]+l_sums[0]+d_sums[0]}<a className = "px-2 text-xl">calories</a>
           </h1>
           <p className = "font-semibold mb-1">Breakfast</p>
           <p class="flex items-center text-gray-600 mb-2">
